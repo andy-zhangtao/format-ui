20c0d7a957f41a0fb2a4baa02d6c1cbf50ae97db
andy <ztao8607@gmail.com>
2020-11-03 20:07:43 +0800

    add text field for nginx soruce content

--- src/compoment/index/topbar.tsx
+++ src/compoment/index/topbar.tsx
@@ -2,24 +2,14 @@ import React, { Suspense } from 'react';
 import { renderRoutes } from 'react-router-config';
 import PropTypes from 'prop-types';
 import clsx from 'clsx';
-import { Link as RouterLink, } from 'react-router-dom';
 import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import PersonIcon from '@material-ui/icons/Person';
-import { Hidden, Link, LinearProgress, Avatar, Typography } from '@material-ui/core';
+import { LinearProgress, Grid, Paper } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
 
 const drawerWidth = 240;
 
@@ -38,7 +28,7 @@ const useStyles = makeStyles((theme: Theme) =>
             }),
         },
         appBarShift: {
-            width: `calc(100% - ${drawerWidth}px)`,
+            width: `calc(100%)`,
             marginLeft: drawerWidth,
             transition: theme.transitions.create(['margin', 'width'], {
                 easing: theme.transitions.easing.easeOut,
@@ -123,67 +113,9 @@ const PersistentDrawerLeft = (props: any) => {
                     >
                         <MenuIcon />
                     </IconButton>
-                    <RouterLink to="/dashboard">
-                        <img
-                            alt="Logo"
-                            src="/images/logos/devex-admin.svg"
-                        />
-                    </RouterLink>
                     <div className={classes.flexGrow} />
-                    <Hidden mdDown>
-                        <Link href="/logout" color="inherit">退出</Link>
-                    </Hidden>
                 </Toolbar>
             </AppBar>
-            <Drawer
-                className={classes.drawer}
-                variant="persistent"
-                anchor="left"
-                open={open}
-                classes={{
-                    paper: classes.drawerPaper,
-                }}
-            >
-                <div className={classes.drawerHeader}>
-                    <IconButton onClick={handleDrawerClose}>
-                        {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-                    </IconButton>
-                </div>
-                <List>
-                    <ListItem >
-
-                        <Avatar
-                            alt="Author"
-                            style={{
-                                marginRight: 2,
-                                backgroundColor: 'orange',
-                                width: theme.spacing(7),
-                                height: theme.spacing(7),
-                            }}
-                            src={avatat}
-                        >
-                        </Avatar>
-                    </ListItem>
-                </List>
-                <Divider />
-                <List>
-                    <ListItem button key='dashboard' component="a" href="/dashboard">
-                        <ListItemIcon>
-                            <DashboardIcon />
-                        </ListItemIcon>
-                        <ListItemText primary='平台总览' />
-                    </ListItem>
-                </List>
-                <Divider />
-                <List>
-                    <ListItem button key='user' component="a" href="/user">
-                        <ListItemIcon>
-                            <PersonIcon />
-                        </ListItemIcon>
-                        <ListItemText primary='用户' />
-                    </ListItem>
-                </List>
-            </Drawer>
             <main
                 className={clsx(classes.content, {
                     [classes.contentShift]: open,
@@ -194,7 +126,27 @@ const PersistentDrawerLeft = (props: any) => {
                         {renderRoutes(route.routes)}
                     </Suspense>
                 </main>
-
+                <div style={{ padding: 2, textAlign: 'center', color: theme.palette.text.secondary }}>
+                    <Grid
+                        container
+                        direction="row"
+                        justify="center"
+                        alignItems="center"
+                    >
+                        <Grid item xs={8}>
+                            <TextField
+                                id="outlined-multiline-flexible"
+                                label="Nginx Conf"
+                                multiline
+                                rowsMax="100"
+                                margin="normal"
+                                helperText="输入nginx配置文件"
+                                variant="outlined"
+                                fullWidth
+                            />
+                        </Grid>
+                    </Grid>
+                </div>
             </main>
         </div>
     );
