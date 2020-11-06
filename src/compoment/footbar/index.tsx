f67f7f23538261d31f36ef5a321859d6c7e13b1e
andy <ztao8607@gmail.com>
2020-11-06 16:01:14 +0800

    添加构建状态和构建时间

--- src/compoment/footbar/index.tsx
+++ src/compoment/footbar/index.tsx
@@ -13,10 +13,15 @@ class FootBarComponent extends Component {
     }
     render() {
         return (
-            <BottomNavigation value={this.state.value} onChange={this.onChange}>
-                <BottomNavigationAction target="_blank" href="mailto:ztao8607@gmail.com" value="ztao8607@gmail.com" label="ztao8607@gmail.com" icon={<MailOutlineIcon />} />
-                <BottomNavigationAction target="_blank" href="https://github.com/andy-zhangtao/format-ui" value="GitHub" label="GitHub" icon={<GitHubIcon />} />
-            </BottomNavigation>
+            <div >
+                <BottomNavigation value={this.state.value} onChange={this.onChange}>
+                    <BottomNavigationAction target="_blank" href="mailto:ztao8607@gmail.com" value="ztao8607@gmail.com" label="ztao8607@gmail.com" icon={<MailOutlineIcon />} />
+                    <BottomNavigationAction target="_blank" href="https://github.com/andy-zhangtao/format-ui" value="GitHub" label="GitHub" icon={<GitHubIcon />} />
+                    <BottomNavigationAction target="_blank" href="https://github.com/andy-zhangtao/format-ui" value="GitHub" label="GitHub" icon={<img src="https://camo.githubusercontent.com/3985736bfd5b8ef06d2f949ecf675214784c8d2bbc145eacc6763d73459ec4ee/68747470733a2f2f7472617669732d63692e636f6d2f616e64792d7a68616e6774616f2f666f726d61742d75692e7376673f6272616e63683d6d61696e" alt="Build Status" data-canonical-src="https://travis-ci.com/andy-zhangtao/format-ui.svg?branch=main" />} />
+                    <BottomNavigationAction icon={process.env.NODE_ENV.concat(' ') + process.env.REACT_APP_BUILD} />
+                </BottomNavigation>
+            </div>
+
 
         )
     }
