a7fc8dd98231b713323540da89e990b5220a0bb9
andy <ztao8607@gmail.com>
2020-11-06 18:15:17 +0800

    显示构建环境

--- src/compoment/footbar/index.tsx
+++ src/compoment/footbar/index.tsx
@@ -18,7 +18,7 @@ class FootBarComponent extends Component {
                     <BottomNavigationAction target="_blank" href="mailto:ztao8607@gmail.com" value="ztao8607@gmail.com" label="ztao8607@gmail.com" icon={<MailOutlineIcon />} />
                     <BottomNavigationAction target="_blank" href="https://github.com/andy-zhangtao/format-ui" value="GitHub" label="GitHub" icon={<GitHubIcon />} />
                     <BottomNavigationAction value="Travis" label="Travis" icon={<img src="https://camo.githubusercontent.com/3985736bfd5b8ef06d2f949ecf675214784c8d2bbc145eacc6763d73459ec4ee/68747470733a2f2f7472617669732d63692e636f6d2f616e64792d7a68616e6774616f2f666f726d61742d75692e7376673f6272616e63683d6d61696e" alt="Build Status" data-canonical-src="https://travis-ci.com/andy-zhangtao/format-ui.svg?branch=main" />} />
-                    <BottomNavigationAction value={process.env.NODE_ENV} icon={process.env.REACT_APP_BUILD} />
+                    <BottomNavigationAction value={process.env.NODE_ENV} label={process.env.NODE_ENV} icon={process.env.REACT_APP_BUILD} />
                 </BottomNavigation>
             </div>
 
